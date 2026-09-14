287a083c743a17dfa4c1f887fbc08e75db923219
Nicolas Duchesne <nicolas.duchesne@edu.devinci.fr>
2026-09-14 11:36:29 +0200

    Test 02 finished

--- Matrix.cs
+++ Matrix.cs
@@ -5,7 +5,7 @@ namespace Maths3D;
 public class Matrix<T> where T : INumber<T>
 {
     //Variables
-    private T[,] _matrix;
+    private T[,] _matrixArray;
     private int _nbLines;
     private int _nbColumns;
     
@@ -16,29 +16,49 @@ public class Matrix<T> where T : INumber<T>
     //Constructors
     public Matrix(int nbLines, int nbColumns)
     {
-        this._matrix = new T[nbLines,nbColumns];
+        this._matrixArray = new T[nbLines,nbColumns];
         this._nbLines = nbLines;
         this._nbColumns = nbColumns;
     }
 
-    public Matrix(T[,] matrix)
+    public Matrix(T[,] array)
     {
-        this._nbLines = matrix.GetLength(0);
-        this._nbColumns = matrix.GetLength(1);
+        this._nbLines = array.GetLength(0);
+        this._nbColumns = array.GetLength(1);
         
-        this._matrix = new T[_nbLines,_nbColumns];
-        this._matrix = matrix;
+        this._matrixArray = new T[_nbLines,_nbColumns];
+        this._matrixArray = array;
+    }
+    
+    public Matrix(Matrix<T> matrix)
+    {
+        this._nbLines = matrix.NbLines;
+        this._nbColumns = matrix.NbColumns;
+        
+        this._matrixArray = new T[_nbLines,_nbColumns];
+        this._matrixArray = matrix.ToArray2D();
     }
 
     //Methods
     public T[,] ToArray2D()
     {
-        return _matrix;
+        T[,] rebuiltArray = new T[_nbLines, _nbColumns];
+
+        for (int i = 0; i < _nbLines; i++)
+        {
+            for (int j = 0; j < _nbColumns; j++)
+            {
+                rebuiltArray[i, j] = this._matrixArray[i, j];
+            }
+        }
+        
+        return rebuiltArray;
     }
     
     //Indexers 
     public T this[int x, int y]
     {
-        get => _matrix[x,y];
+        get => _matrixArray[x,y];
+        set => _matrixArray[x,y] = value;
     }
 }
