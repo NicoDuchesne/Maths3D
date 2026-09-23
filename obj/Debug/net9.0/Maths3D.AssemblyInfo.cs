98ec6a11a75411b541f157d82d956b76fc830d31
Nicolas Duchesne <nicolas.duchesne@edu.devinci.fr>
2026-09-23 11:46:56 +0200

    Test 19 solution files

--- obj/Debug/net9.0/Maths3D.AssemblyInfo.cs
+++ obj/Debug/net9.0/Maths3D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maths3D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7300f62e8581526d3579a90ce6168f071da90bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83058d1c5809ed763373ac0404c4b915fa06f912")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maths3D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maths3D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
